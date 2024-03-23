6ee0db29020d8321b72a2b7df074a1a4f321a894
kirisaki-vk <kirisaki.vk@gmail.com>
2024-03-23 16:54:46 +0300

    chore: added import alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ import generouted from '@generouted/react-router/plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), generouted()],
+  resolve: { alias: { '@': '/src' } },
+  server: {
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.96:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  },
 })
