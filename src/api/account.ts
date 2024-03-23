eb48260f2d8e019640781ec751f5d71855a3ebd8
kirisaki-vk <kirisaki.vk@gmail.com>
2024-03-23 16:09:19 +0300

    feat: fetcher account.ts

--- src/api/account.ts
+++ src/api/account.ts
@@ -1,11 +1,29 @@
 import axiosClient from "./client";
 
 export type Account = {
-    name: string;
-    email: string;
+    firstname: string;
+    lastname:string;
+    accountNumber: number;
+    salary: number;
+    type: "NORMAL" | "DEBIT";
+    birthdate: string;
     id: number;
 }
 
+export type NewAccount = Partial<Account>
+
 export function getAllAccounts(): Promise<Account[]> {
-    return axiosClient.get("/users").then(res => JSON.parse(res.data));
+    return axiosClient.get("/account").then(res => JSON.parse(res.data));
+}
+
+export function getAccountById(accountId: number): Promise<Account> {
+    return axiosClient.get(`/account/${accountId}`).then(res => JSON.parse(res.data));
+}
+
+export function createAccount(account: NewAccount): Promise<Account> {
+    return axiosClient.post(`/account`, JSON.stringify(account), {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => JSON.parse(res.data));
 }
