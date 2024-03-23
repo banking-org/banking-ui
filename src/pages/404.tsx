a1259ffb5be79828da61195166debb81e7e07be2
kirisaki-vk <kirisaki.vk@gmail.com>
2024-03-23 17:26:37 +0300

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import { Flex, Text, Heading } from "@chakra-ui/react";
+
+export default function NotFound() {
+    return <Flex w={"100%"} h={"100%"} justifyContent={"center"} alignItems={"center"}>
+        <Text fontSize={"x-large"}>404 | Page not found</Text>
+    </Flex>
+}
