e9cd93342c505bf0f98b647e952cc8f2ff27a4c9
kirisaki-vk <kirisaki.vk@gmail.com>
2024-03-24 18:05:19 +0300

    chore: updated balance api wrapper

--- src/api/balance.ts
+++ src/api/balance.ts
@@ -7,6 +7,46 @@ export type Balance = {
     withdraws: number;
     interests: number;
 }
+
+export type Transaction = {
+    id: number;
+    amount: number;
+    label: string;
+    effectDate: Date;
+    type?: "DEBIT" | "CREDIT";
+}
+
+export type TransactionResponse = {
+    account: Account;
+    currentAmount: number;
+}
+
 export function getBalanceByAccountId(accountId: number): Promise<Balance> {
     return axiosClient.get(`/balance/${accountId}`).then(res => JSON.parse(res.data));
+}
+
+export function deposit(accountId: number, transaction: Omit<Transaction, "id">): Promise<TransactionResponse> {
+    return axiosClient.put("/transaction/deposit", JSON.stringify({
+        accountId,
+        ...transaction
+    }), {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => JSON.parse(res.data));
+}
+
+export function getAllTransactions(accountId: number): Promise<Transaction[]> {
+    return axiosClient.get(`/transaction/${accountId}`).then(res => JSON.parse(res.data));
+}
+
+export function doWithdraw(accountId: number, amount: number) {
+    return axiosClient.put("/transaction/withdraw", JSON.stringify({
+        accountId,
+        amount
+    }), {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => JSON.parse(res.data));
 }
