5cbba05e153da8a2e9f4c489f71106c8cef7317b
kirisaki-vk <kirisaki.vk@gmail.com>
2024-03-24 18:03:56 +0300

    chore: globalized queryProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import {Outlet} from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
     return (
@@ -6,7 +9,9 @@ export default function App() {
             width: "100vw",
             height: "100vh"
         }}>
-            <Outlet/>
+            <QueryClientProvider client={queryClient}>
+                <Outlet/>
+            </QueryClientProvider>
         </main>
     )
 }
