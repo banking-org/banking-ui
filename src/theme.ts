4fece58611e4c9e74c061944bddd97dc53f869fe
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:21:17 +0300

    chore: added datepicker theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,8 @@
 import {extendTheme} from "@chakra-ui/react";
+import {CalendarDefaultTheme} from "@uselessdev/datepicker"
 import '@fontsource-variable/rubik'
 
-const theme = extendTheme({
+const theme = extendTheme(CalendarDefaultTheme ,{
     fonts: {
         heading: `'Rubik Variable', sans-serif`,
         body: `'Rubik Variable', sans-serif`
