4cc03a414a3aefd188e8de4156891fedeecade5d
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:25:33 +0300

    feat: edit interest form

---
+++ src/pages/dashboard/[accountId]/edit/_components/EditInterest.tsx
@@ -0,0 +1,26 @@
+import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { getInterest } from "@/api/interest.ts";
+import { getAccountById } from "@/api/account.ts";
+
+export default function EditInterest({ accountId }: { accountId: number }) {
+    const { data: account } = useQuery({
+        queryKey: ["getAccountById"],
+        queryFn: () => getAccountById(+accountId),
+    });
+    const { data } = useQuery({
+        queryKey: ["getInterest"],
+        queryFn: () => getInterest(accountId),
+    });
+
+    return account?.accountType === "DEBIT" ? <Box w={"100%"}>
+        <FormControl>
+            <FormLabel>First interest</FormLabel>
+            <Input placeholder={data?.firstInterest.toString()} />
+        </FormControl>
+        <FormControl>
+            <FormLabel>Effective interest</FormLabel>
+            <Input placeholder={data?.effectiveInterest.toString()} />
+        </FormControl>
+    </Box> : <></>;
+}
