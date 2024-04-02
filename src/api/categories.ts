168174658d831f1c58b0a5ef522c349f9758ca5c
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:01:51 +0300

    chore: added categories api wrapper

---
+++ src/api/categories.ts
@@ -0,0 +1,17 @@
+import axiosClient from "@/api/client.ts";
+
+type Category = {
+    id: number;
+    name: string;
+    comments: string;
+    onlyOn: "DEBIT" | "CREDIT";
+}
+
+
+export function getCategoriesByType(transactionType: "DEBIT" | "CREDIT"): Promise<Category[]>{
+    return axiosClient.get("/categories", {
+        params: {
+            type: transactionType
+        }
+    }).then(res => JSON.parse(res.data));
+}
