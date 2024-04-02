ba2f5061adea551ec7e5d629855578b5dfb4b484
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:12:09 +0300

    feat: interest API wrapper

---
+++ src/api/interest.ts
@@ -0,0 +1,12 @@
+import axiosClient from "@/api/client.ts";
+
+export type Interest = {
+    id: number;
+    firstInterest: number;
+    effectiveInterest: number;
+    maxDaysPayment: number;
+}
+
+export function getInterest(accountId: number): Promise<Interest> {
+    return axiosClient.get(`/interest/${accountId}`).then(res => JSON.parse(res.data))
+}
