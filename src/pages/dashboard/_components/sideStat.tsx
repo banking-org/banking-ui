c508c8415fc496656593165ef364e8de1fca1850
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:17:57 +0300

    chore: format

--- src/pages/dashboard/_components/sideStat.tsx
+++ src/pages/dashboard/_components/sideStat.tsx
@@ -1,22 +1,22 @@
-import {Box, Stat, StatArrow, StatGroup, StatHelpText, StatLabel, StatNumber} from "@chakra-ui/react";
-import {useQuery} from "@tanstack/react-query";
-import {getBalanceByAccountId} from "@/api/balance.ts";
-import {useEffect} from "react";
+import { Box, Stat, StatArrow, StatGroup, StatHelpText, StatLabel, StatNumber } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { getBalanceByAccountId } from "@/api/balance.ts";
+import { useEffect } from "react";
 
 type SideStatProps = {
     accountId: number;
     show: boolean;
 }
 
-export default function SideStat({accountId, show}: SideStatProps) {
-    const {data, isLoading, error} = useQuery({
-        queryKey: ['getBalance' + accountId],
-        queryFn: () => getBalanceByAccountId(accountId)
-    })
+export default function SideStat({ accountId, show }: SideStatProps) {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["getBalanceSide"],
+        queryFn: () => getBalanceByAccountId(accountId),
+    });
 
     useEffect(() => {
         console.log(data);
-    }, [data])
+    }, [data]);
     return <Box
         w={"100%"}
         bgColor={"gray.200"}
@@ -33,7 +33,7 @@ export default function SideStat({accountId, show}: SideStatProps) {
         </Stat>
         <StatGroup>
             <Stat>
-                <StatLabel><StatArrow type='increase' transform={"rotate(180deg)"}/>Income</StatLabel>
+                <StatLabel><StatArrow type="increase" transform={"rotate(180deg)"} />Income</StatLabel>
                 <StatNumber noOfLines={1}>345,670 $</StatNumber>
                 <StatHelpText>
                     This month
@@ -41,9 +41,9 @@ export default function SideStat({accountId, show}: SideStatProps) {
             </Stat>
 
             <Stat>
-                <StatLabel><StatArrow type='decrease' transform={"rotate(180deg)"}/>Outcome</StatLabel>
+                <StatLabel><StatArrow type="decrease" transform={"rotate(180deg)"} />Outcome</StatLabel>
                 <StatNumber>45 $</StatNumber>
             </Stat>
         </StatGroup>
-    </Box>
+    </Box>;
 }
