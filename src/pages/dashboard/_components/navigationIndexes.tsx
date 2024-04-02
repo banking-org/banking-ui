ff9dbf6bbb5d08e0a46e55d475a907edbfcabdda
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:15:52 +0300

    chore: updates navigation Indexes to include new routes

--- src/pages/dashboard/_components/navigationIndexes.tsx
+++ src/pages/dashboard/_components/navigationIndexes.tsx
@@ -1,6 +1,9 @@
 import { Params, Path } from "@/router";
-import { ReactNode } from "react"
-import {GoArrowBoth, GoHome} from "react-icons/go";
+import React, { ReactNode } from "react";
+import { GoHome } from "react-icons/go";
+import { GiMoneyStack } from "react-icons/gi";
+import { MdTransferWithinAStation } from "react-icons/md";
+import { GrDocumentPerformance } from "react-icons/gr";
 
 export type Navigation<T extends Path> = {
     icon: ReactNode;
@@ -11,21 +14,43 @@ export type Navigation<T extends Path> = {
     }
 }
 
-export const navigations= (accountId: string | number): Navigation<Path>[] => [
+export const navigations = (accountId: string | number): Navigation<Path>[] => [
     {
         icon: <GoHome size={"25px"} />,
         label: "Home",
         path: {
             url: "/dashboard/:accountId",
-            params: {accountId: accountId + ""}
-        }
+            params: { accountId: accountId + "" },
+        },
     },
     {
-        icon: <GoArrowBoth size={"25px"}/>,
-        label: "Transfers",
+        icon: <GiMoneyStack size={"25px"} />,
+        label: "Debts",
         path: {
-            url: "/dashboard/:accountId/transfers",
-            params: {accountId: accountId + ""}
+            url: "/dashboard/:accountId/debts",
+            params: {
+                accountId: accountId + "",
+            },
+        },
+    },
+    {
+        icon: <GrDocumentPerformance />,
+        label: "Statement",
+        path: {
+            url: "/dashboard/:accountId/statement",
+            params: {
+                accountId: accountId + "",
+            },
+        },
+    },
+    {
+        icon: <MdTransferWithinAStation />,
+        label: "Grouped Transfer",
+        path: {
+            url: "/dashboard/:accountId/grouped-transfers",
+            params: {
+                accountId: accountId + ""
+            }
         }
     },
-]
+];
