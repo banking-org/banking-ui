25ad9ed8b31633bbf329cccb2a2ba038ea7ff0a3
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:13:47 +0300

    feat: statement API wrapper

---
+++ src/api/satement.ts
@@ -0,0 +1,21 @@
+import axiosClient from "@/api/client.ts";
+
+type Statement = {
+    date: Date;
+    reference: string;
+    label; string;
+    credit: number;
+    debit: number;
+    balance: number;
+}
+
+type StatementDateRange = {
+    start: Date | string;
+    end: Date | string;
+}
+
+export function getStatement(accountId: string ,range: StatementDateRange): Promise<Statement[]> {
+    return axiosClient.get(`/statement/${accountId}`, {
+        params: range
+    }).then(res => JSON.parse(res.data))
+}
