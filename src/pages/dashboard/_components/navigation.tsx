19f21cfa068cb3f23b95c365aa525cdf5f0198e0
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:14:58 +0300

    refactor: navigation bar

--- src/pages/dashboard/_components/navigation.tsx
+++ src/pages/dashboard/_components/navigation.tsx
@@ -1,39 +1,23 @@
-import {useNavigate, useParams} from "@/router"
-import {Avatar, Button, Flex, Heading, Skeleton, Text} from "@chakra-ui/react";
+import { useNavigate, useParams } from "@/router";
+import { Avatar, Button, Flex, Heading, Skeleton, Text } from "@chakra-ui/react";
 import SideStat from "./sideStat";
-import {useQuery} from "@tanstack/react-query";
-import {getAccountById} from "@/api/account";
 import NavigationLink from "./navigationLink";
-import {useAtom} from "jotai";
-import {navigationAtom} from "../_atoms/navigationAtoms";
-import {navigations} from "@/pages/dashboard/_components/navigationIndexes.tsx";
-import {MdOutlineExitToApp} from "react-icons/md";
+import { useAtom } from "jotai";
+import { navigationAtom } from "../_atoms/navigationAtoms";
+import { navigations } from "@/pages/dashboard/_components/navigationIndexes.tsx";
+import { MdOutlineExitToApp } from "react-icons/md";
+import AccountHeader from "@/pages/dashboard/_components/accountHeader.tsx";
 
 export default function Navigation() {
+    const { accountId } = useParams("/dashboard/:accountId");
     const [navigation, setNavigation] = useAtom(navigationAtom);
-    const {accountId} = useParams("/dashboard/:accountId");
-    const {data, isLoading, error} = useQuery({
-        queryKey: ['getAccountById'],
-        queryFn: () => getAccountById(+accountId)
-    });
     const navigate = useNavigate();
     return <Flex direction={"column"} justifyContent={"space-between"} h={"100%"}>
         <Flex flexDir={"column"} alignItems={"center"} p={3} gap={4}>
-            <Flex w={"100%"} alignItems={"center"} gap={3}>
-                <Avatar name={`${data?.firstname || ""} ${data?.lastname || ""}`}/>
-                <Flex flexDir={"column"} justifyContent={"center"} h={"100%"}>
-                    <Skeleton isLoaded={!isLoading}>
-                        <Heading as={"h3"} size={"sm"}>
-                            {data ? `${data?.firstname} ${data?.lastname}` : ""}
-                        </Heading>
-                    </Skeleton>
-                    <Skeleton isLoaded={!isLoading} mt={isLoading ? "2" : ""}>
-                        <Text>{data?.accountNumber}</Text>
-                    </Skeleton>
-                </Flex>
-            </Flex>
 
-            <SideStat show={navigation !== 0} accountId={+accountId}/>
+            <AccountHeader accountId={+accountId} />
+
+            <SideStat show={navigation !== 0} accountId={+accountId} />
 
             <Flex w={"100%"} flexDir={"column"} gap={2}>
                 {
@@ -41,7 +25,7 @@ export default function Navigation() {
                         // @ts-expect-error This type should not fail but typescript force me to do it
                         <NavigationLink
                             onClick={() => {
-                                setNavigation(i)
+                                setNavigation(i);
                             }}
                             active={navigation === i}
                             key={i}
@@ -50,7 +34,7 @@ export default function Navigation() {
                             params={nav.path.params}
                         >
                             {nav.label}
-                        </NavigationLink>
+                        </NavigationLink>,
                     )
                 }
             </Flex>
@@ -60,11 +44,11 @@ export default function Navigation() {
             m={2}
             leftIcon={<MdOutlineExitToApp />}
             onClick={() => {
-                navigate("/")
-                setNavigation(0)
+                navigate("/");
+                setNavigation(0);
             }}
         >
             Leave
         </Button>
-    </Flex>
+    </Flex>;
 }
