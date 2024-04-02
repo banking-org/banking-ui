ca225bf6daae05ddba2f703a8eac4783952c9c44
kirisaki-vk <kirisaki.vk@gmail.com>
2024-04-02 13:17:30 +0300

    feat: updated routes

--- src/router.ts
+++ src/router.ts
@@ -8,14 +8,18 @@ export type Path =
   | `/auth`
   | `/dashboard/:accountId`
   | `/dashboard/:accountId/debts`
+  | `/dashboard/:accountId/edit`
+  | `/dashboard/:accountId/grouped-transfers`
   | `/dashboard/:accountId/history`
-  | `/dashboard/:accountId/transactions`
+  | `/dashboard/:accountId/statement`
 
 export type Params = {
   '/dashboard/:accountId': { accountId: string }
   '/dashboard/:accountId/debts': { accountId: string }
+  '/dashboard/:accountId/edit': { accountId: string }
+  '/dashboard/:accountId/grouped-transfers': { accountId: string }
   '/dashboard/:accountId/history': { accountId: string }
-  '/dashboard/:accountId/transactions': { accountId: string }
+  '/dashboard/:accountId/statement': { accountId: string }
 }
 
 export type ModalPath = never
