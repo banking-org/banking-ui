986619910a694c2fce3825f91356ad3e8602d456
Abi-Nf <hei.abigail.3@gmail.com>
2024-04-02 21:31:09 -0400

    feat: Dash container

---
+++ src/pages/dashboard/[accountId]/_components/Graph.tsx
@@ -0,0 +1,21 @@
+import { CommonData, getSumDebitsAndCreditsData } from "@/api/dashboard.ts";
+import { useQuery } from "@tanstack/react-query";
+import AccountGraph from "./accountGraph";
+
+interface Props {
+    accountId: number;
+}
+
+export const Graph = ({ accountId }: Props) => {
+    const { data } = useQuery({
+        queryKey: [ "graph-debits-and-credits" ],
+        queryFn: () => getSumDebitsAndCreditsData(accountId)
+    });
+
+    return (
+        <>
+            <AccountGraph data={data?.incomes || {} as CommonData} graphType={"income"}/>
+            <AccountGraph data={data?.outcomes || {} as CommonData} graphType={"income"}/>
+        </>
+    )
+}
